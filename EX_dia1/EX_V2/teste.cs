d0153a059cb67430bd6a19d8a88454b881fe394b
Arthur Rosa Ferreira <arthur.rosa05@edu.pucrs.br>
2025-10-17 14:09:38 -0300

    Add files via upload

---
+++ EX_dia1/EX_V2/teste.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+
+
+
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+
+
+
+
+        //DIA 1 EX 5
+        /* 
+        int cap_max = 1000;
+        int ing_ven = 75;
+        int ing_res = cap_max - ing_ven;
+
+
+        Console.WriteLine("Ingresso vendido " + ing_ven + " de " + cap_max);
+        Console.WriteLine("Ingressos vendidos " + ing_res);
+        */
+
+        /*
+
+        //DIA 1 EX 6.1
+
+
+
+        //char teste = '😊'; 
+        //Too many characters in character literalCS1012
+
+        char teste = '☺';
+
+        Console.WriteLine(teste);
+
+        */
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*
+
+        //DIA 1 EX 6.2
+
+        char letra = 'A';
+        int letras = 'A';
+        Console.WriteLine(letra);
+        Console.WriteLine("A = " + letras);// valor da tabela ASCII???
+        */
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*
+
+        //DIA 1 EX 6.3
+
+        String teste = "TRUE";
+
+        bool verdade = Convert.ToBoolean(teste);
+
+        Console.WriteLine(teste); // QUAL A DIF ENTRE COM SYS E SEM SYS
+        */
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        /*
+
+        //DIA 1 6.4
+
+        Double pontos = 1020.3;
+
+        int balas = System.Convert.ToInt32(pontos);  
+
+        Console.WriteLine(balas);
+
+        */
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+    }
+
+}
+        
+        
+        
+        
+
+
+
